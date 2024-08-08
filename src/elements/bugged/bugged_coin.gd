29a6bdf680469b0e7e736d6ca59cae21c11bea60
Luca Pasini <93383070+LukePasax@users.noreply.github.com>
2024-08-08 18:52:15 +0200

    Add Bugged Coin

---
+++ src/elements/bugged/bugged_coin.gd
@@ -0,0 +1,15 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func pick_up(body: Node2D):
+	if body is Player:
+		queue_free()
